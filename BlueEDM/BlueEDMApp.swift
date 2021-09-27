634644b414c56ff2a84277b406c6e9402f73d611
cp@staszkiewic.de <cp@staszkiewicz.de>
2021-09-27 22:03:24 +0200

    Initial Commit

---
+++ BlueEDM/BlueEDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueEDMApp.swift
+//  BlueEDM
+//
+//  Created by Staszkiewicz, Carl Philipp on 27.09.21.
+//
+
+import SwiftUI
+
+@main
+struct BlueEDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
